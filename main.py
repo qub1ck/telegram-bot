76424ea116bce2e421a080ce7923f9be4c9d7539
qub1ck <116922015+qub1ck@users.noreply.github.com>
2025-03-20 11:36:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def start_search():
                 # Start new job
                 telegram_app.job_queue.run_repeating(
                     check_dates_continuously,
-                    interval=300,
+                    interval=60,
                     first=0,
                     data={'chat_id': user_id, 'user_choice': original_option, 'user_id': user_id, 'job_name': job_name},
                     name=job_name_to_run,
@@ -852,7 +852,7 @@ async def restart_active_jobs(app: Application):
         job_name_to_run = f"check_dates_{user_id}_{job_name}"
         app.job_queue.run_repeating(
             check_dates_continuously,
-            interval=300,
+            interval=60,
             first=5,
             data={'chat_id': user_id, 'user_choice': original_option_text, 'user_id': user_id, 'job_name': job_name},
             name=job_name_to_run,
@@ -912,7 +912,7 @@ async def check_for_new_jobs(context: CallbackContext):
             try:
                 context.job_queue.run_repeating(
                     check_dates_continuously,
-                    interval=300,
+                    interval=60,
                     first=5,
                     data={
                         'chat_id': user_id,
@@ -952,7 +952,7 @@ async def on_startup(app: Application):
     # Add a job to check for new active jobs periodically
     app.job_queue.run_repeating(
         check_for_new_jobs,
-        interval=300,  # Check every 30 seconds
+        interval=60,  # Check every 30 seconds
         first=5,  # Start checking after 5 seconds
         name="check_for_new_jobs",
         job_kwargs={'max_instances': 2}
